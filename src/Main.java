8d5082316a1c3a79858aad73dcce3a332ab60bb5
Moeen <smnehzati@gmail.com>
2025-02-02 19:12:08 -0500

    first two weeks

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,69 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package src;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+//        Let's work here for a while
+//        A program is run one line at a time
+//        Unless started with "//" All lines finish with a special character, either ";" or "}"
+//        Lines that start with // will be overlooked by the compiler
+//        In general excess white space does not matter, it's there for our own sake
+//        Create a pointer to some value called name, its value is a string of characters "Omeed"
+        //this is declaration
+        String name;
+//        this is variable assignment
+        name = "original value";
+//        you can have multiple assignment
+        System.out.println("value of the variable name is:" + name);
+        name = "secondary value";
+        System.out.println("value of the variable name is:" + name);
+        name = "teritary value";
+        System.out.println("value of the variable name is:" + name);
+//        But you cant have multiple declarations
+//        String name;
+        String surname = "Mahdavi";
+        System.out.println(name + " " + surname);
+        //you've seen what Strings are
+        //now for numbers
+//        these don't give the same result
+        System.out.println("12" + "12");
+        System.out.println(12 + 12);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        int integer;
+        integer = 1;
+        System.out.println(integer*100 + 1);
+
+//        There are many different types of variables:
+//        char: a single character
+        char character = 'a';
+//        double: A rational number
+        double doubleValue = 3.22;
+        System.out.println(doubleValue);
+
+//        All of these are distinct! "3", '3', 3, 3.0
+//      Functions are sensitive to the type of their arguments
+//      function is a piece of code that that you call to run something
+//      examples can be main, System.out.println
+//        sensitive to input as in "12" * 2; causing an error
+        String joined = String.join(",", "first", "second", "third");
+        System.out.println(joined);
+//        This will not work though
+//        String.join(1, "first", "second", "third");
     }
 }
+
+//
+//class Explanations {
+//    public static void main(String[] args) {
+////        Though barely legible, this is a language designed for us people to understand and not the computers themselves
+////        All we do here is make explicit what we want from our system to do
+////        There is a program called the "compiler" that translates this to something closed to what a machine may understand
+////        That's called "Byte Code", which is generated as "*.class" files
+////        To run a code, we pass the byte code to another program called the "Virtual Machine"(VM). VM is the interface between the byte code and the computer.
+////        In this example, "Javac src/Main.java" will create the byte code
+////        "Java src.Main" runs the created byte code
+//    }
+//}
--- src/TakingInputs.java
+++ src/TakingInputs.java
@@ -1,2 +1,26 @@
+import java.util.Scanner;
+
 public class TakingInputs {
+    public static void main(String[] args) {
+//        Declaring a variable of type Scanner
+        Scanner sc;
+//        defines the value assigned to sc
+        sc = new Scanner(System.in);
+//       prompt the user for information
+        System.out.println("say something");
+//        Waits for when the user enters a line by pressing enter
+        String input = sc.nextLine();
+        System.out.println("you said: "+ input);
+        System.out.println("give me an integer so I can multiply it by two");
+//        Can't say this
+//        int num = sc.nextLine();
+//        You can:
+        int num = sc.nextInt();
+        System.out.println("result is "+ num*2);
+    }
 }
+
+
+//
+//For next week:
+//Ask for name and age, print their year of birth
